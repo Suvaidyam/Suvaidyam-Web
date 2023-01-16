7e780d6cc51ad72cb02cbba349cd27d3e6fb814e
sujeetsuvaidyam <sujeet.suvaidyam@gmail.com>
2023-01-16 16:50:37 +0530

    modified team and about components

--- src/Components/AboutUs/Card.js
+++ src/Components/AboutUs/Card.js
@@ -7,30 +7,30 @@ export const Card = () => {
     return (
         <>
             {/* Empower */}
-            <div className=' flex justify-end lg:w-[510px] mt-16'>
+            <div className=' flex lg:justify-end lg:ml-0 ml-7  lg:w-[510px] lg:mt-16 mt-8 text-bgblue'>
                 <div>
-                    <h1 className='text-xl'>HOW WE WORK </h1>
-                    <h1 className='text-5xl mt-4 from-neutral-400'>We EMPOWER</h1>
+                    <h1 className='lg:text-xl text-[18px] font-semibold'>HOW WE WORK </h1>
+                    <h1 className='lg:text-5xl text-4xl lg:mt-4 mt-2 from-neutral-400'>We EMPOWER</h1>
                 </div>
             </div>
             {/* card guide */}
-            <div className='lg:flex justify-center mt-16'>
-                <div className='bg-gray-50 w-[480px] mx-3 lg:flex pb-7 items-center'>
-                    <img src={encourage} alt="encourage" className='rounded-full px-6 py-5' />
-                    <div className='mt-5'>
+            <div className='lg:flex justify-center mt-16 lg:w-full '>
+                <div className='bg-gray-50 lg:w-[480px] w-full lg:mx-3 lg:flex pb-7 items-center'>
+                    <img src={encourage} alt="encourage" className='rounded-full lg:w-48  w-[170px]  px-6 py-5' />
+                    <div className='lg:mt-5 lg:ml-0 ml-7'>
                         <p className='text-2xl'>WE ENCOURAGE</p>
-                        <p className='mt-3 text-gray-400  text-[14px] font-thin w-[230px] '>Where a child is born has a huge
+                        <p className='mt-3 text-gray-400  text-[14px] font-thin lg:w-[230px] w-[260px] '>Where a child is born has a huge
                             influence over their education
                             and career trajectories. We are brining tech exposure and opportunities
                             to areas where even access to stable internet is a challenge.
                         </p>
                     </div>
                 </div>
-                <div className='bg-gray-50 w-[480px] mx-3 lg:flex pb-7 items-center'>
-                    <img src={books} alt="Books" className='rounded-full lg:px-6 lg:py-5' />
-                    <div className='mt-5'>
+                <div className='bg-gray-50 lg:w-[480px] lg:mx-3 lg:mt-0 mt-4 lg:flex pb-7 items-center'>
+                    <img src={books} alt="Books" className='rounded-full lg:w-48 w-[170px] px-6 py-5' />
+                    <div className='lg:mt-5 lg:ml-0 ml-7'>
                         <p className='text-2xl'>WE GUIDE</p>
-                        <p className='mt-3 text-gray-400  text-[14px] font-thin w-[220px]'>Along with providing quality
+                        <p className='mt-3 text-gray-400  text-[14px] font-thin lg:w-[220px] w-[260px]'>Along with providing quality
                             computer education in rural areas
                             where most don't have access to it growing up, we are imparting end-to-end guidance
                             on how to launch successful careers in tech.
@@ -39,18 +39,18 @@ export const Card = () => {
                 </div>
             </div>
             <div className='lg:flex justify-center mt-4'>
-                <div className='bg-gray-50 w-[480px] mx-3 lg:flex pb-7 items-center '>
-                    <img src={desktop} alt="encourage" className='rounded-full px-6 py-5 h-[200px]' />
-                    <div className=''>
+                <div className='bg-gray-50 lg:w-[480px] lg:mx-3 lg:flex pb-7 items-center '>
+                    <img src={desktop} alt="encourage" className='rounded-full lg:w-48 w-[170px] px-6 py-5' />
+                    <div className=' lg:ml-0 ml-7'>
                         <p className='text-2xl'>WE INVEST</p>
-                        <p className='mt-3 text-gray-400  text-[14px] font-thin w-[240px]'>Introduce your team! Click here to add images,
+                        <p className='mt-3 text-gray-400  text-[14px] font-thin lg:w-[240px] w-[260px]'>Introduce your team! Click here to add images,
                             text and links, or connect data from your collection.
                         </p>
                     </div>
                 </div>
-                <div className='bg-gray-50 w-[480px] mx-3 lg:flex pb-7 items-center'>
-                    <img src={board} alt="encourage" className='rounded-full px-6 py-5 h-[200px]' />
-                    <div className=''>
+                <div className='bg-gray-50 lg:w-[480px] lg:mx-3 lg:mt-0 mt-4 lg:flex pb-7 items-center'>
+                    <img src={board} alt="encourage" className='rounded-full lg:w-48 w-[170px] px-6 py-5' />
+                    <div className=' lg:ml-0 ml-7 '>
                         <p className='text-2xl'>WE FACILITATE</p>
                         <p className='mt-3 text-gray-400  text-[14px] font-thin w-[240px]'>Introduce your team! Click here to add images,
                             text and links, or connect data from your collection.
--- src/Components/Teams/Teams.js
+++ src/Components/Teams/Teams.js
@@ -2,7 +2,34 @@ import React from 'react'
 
 const Teams = () => {
   return (
-    <div>Hii Teams</div>
+    <>
+      {/* founder */}
+      <div className='bg-bgblue w-full h-[610px] flex gap-20 '>
+        <div className='w-[640px] flex justify-end mt-24'>
+          <div className='w-[395px]'>
+            <p className='text-white text-[40px] font-normal tracking-[3px]'>Amresh Kumar</p>
+            <p className='text-white font-fontsans font-semibold text-[13px] tracking-[2px] mt-3'>Founder</p>
+            <p className='text-white mt-16'>I’m a paragraph. Use this space to tell people more about what you do and the services
+              you offer. Double click here or click Edit Text to get started. Te about what you
+              do and the services you offer. Double click here or click Edit Text to get started.</p>
+            <p className='text-white mt-12'>
+              I’m a paragraph. Use this space to tell people more about what you do and the services you offer.
+              Double click here or click Edit Text to get started. I’m a para. Use this space to.
+            </p>
+          </div>
+        </div>
+        <div className='bg-gray-300 w-96 h-[500px] mt-24 '>
+        </div>
+      </div>
+      {/* Team */}
+      <div className='text-center  font-sans mt-16'>
+        <div className='opacity-75'>
+          <p className='text-[45px]'>TEAM</p>
+          <p className='text-5xl'>SUVAIDYAM</p>
+        </div>
+        <hr className='w-8 m-auto bg-black h-1 mt-7' />
+      </div>
+    </>
   )
 }
 

1c507b5c9d0dcb8ec7428adc865bb712a230f3ad
sujeetsuvaidyam <sujeet.suvaidyam@gmail.com>
2023-01-18 17:31:44 +0530

    modified team

--- src/Components/Common/Header/Navbar.js
+++ src/Components/Common/Header/Navbar.js
@@ -7,19 +7,19 @@ const Navbar = () => {
 
     const [openNav, setOpenNav] = useState(false)
 
-    const nav =[
-        {name:'Home',to:'/',click:false},
-        {name:'About Us',to:'/about',click:false},
-        {name:'The Team',to:'/teams',click:false},
-        {name:'Contact Us',to:'/contact',click:false}
+    const nav = [
+        { name: 'Home', to: '/', click: false },
+        { name: 'About Us', to: '/about', click: false },
+        { name: 'The Team', to: '/teams', click: false },
+        { name: 'Contact Us', to: '/contact', click: false }
     ]
     return (
         <>
             <div className="hidden lg:block">
                 <div className="flex gap-8 items-center">
                     <div className='flex gap-6'>
-                        {nav.map(items=>(
-                             <NavLink to={items.to} className='font-semibold text-[15px] hover:text-slate-500
+                        {nav.map(items => (
+                            <NavLink to={items.to} className='font-semibold text-[15px] hover:text-slate-500
                              text-[#0C1D39]'>{items.name}</NavLink>
                         ))}
                     </div>
@@ -33,13 +33,13 @@ const Navbar = () => {
             <div className="block lg:hidden ">
                 {openNav === false ? <HiBars3 className='text-4xl cursor-pointer' onClick={() => setOpenNav(!openNav)} /> :
                     <IoMdClose className='text-4xl cursor-pointer' onClick={() => setOpenNav(!openNav)} />}
-                 {openNav === true ?
+                {openNav === true ?
                     <div className="w-3/5 h-screen bg-[#0C1D39] absolute left-0 top-0">
                         <div className='w-full flex flex-col gap-1 pt-20 px-8'>
-                        {nav.map(items=>(
-                           <NavLink to={items.to} className='font-light tracking-widest text-[20px] hover:text-red-600
-                           text-white border-b w-4/5 h-12' onClick={()=>setOpenNav(items.click)}>{items.name}</NavLink>
-                        ))}
+                            {nav.map(items => (
+                                <NavLink to={items.to} className='font-light tracking-widest text-[20px] hover:text-red-600
+                           text-white border-b w-4/5 h-12' onClick={() => setOpenNav(items.click)}>{items.name}</NavLink>
+                            ))}
                         </div>
                     </div> : null}
 

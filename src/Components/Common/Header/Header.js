ab6f138df66d38a6e94266aea8f1eff847062715
ani07ket <aniket.suvaidyam@gmail.com>
2023-01-19 16:46:23 +0530

    image slider in about us

--- src/Components/Common/Header/Header.js
+++ src/Components/Common/Header/Header.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Logo from '../../../Assets/Logo.png'
-import Navbar from './Navbar'
+import React from "react";
+import Logo from "../../../Assets/Logo.png";
+import Navbar from "./Navbar";
 
 const Header = () => {
   return (
     <>
-       <div className="w-full max-w-[1720px] mx-auto h-20 flex px-5 lg:px-20 justify-between fixed top-0 bg-white z-50">
+      <div className="w-full max-w-[1720px] mx-auto h-20 flex px-5 lg:px-20 justify-between fixed top-0 bg-white z-[3000]">
         {/* Logo */}
-          <div className=" h-full flex items-center ">
-            <img className='w-3/5 lg:w-4/5' src={Logo} alt="Logo" />
-          </div>
-          <div className=" h-full flex  items-center justify-center">
-            <Navbar/>
-          </div>
-        </div> 
+        <div className=" h-full flex items-center ">
+          <img className="w-3/5 lg:w-4/5" src={Logo} alt="Logo" />
+        </div>
+        <div className=" h-full flex  items-center justify-center">
+          <Navbar />
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;

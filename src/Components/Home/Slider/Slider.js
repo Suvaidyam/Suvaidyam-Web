e1a1741357eaf5d80329475be05f650d3bb6e440
abhiSuvaidyam <abhi.suvaidyam@gmail.com>
2023-01-30 16:28:49 +0530

    add validation in contact page

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import home from '../../../Assets/home-img.webp'
 
 const Slider = () => {
+  const homeSlider = [
+    {
+      url: "https://static.wixstatic.com/media/89ec68_f16e2fa1d2cb4eeba61e49ff0e51a299~mv2.jpg/v1/fill/w_882,h_601,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/89ec68_f16e2fa1d2cb4eeba61e49ff0e51a299~mv2.jpg"
+    },
+  ]
   return (
     <>
-    <div><img src={home} alt="img" className='w-full h-[380px] md:h-[600px] ' /></div>
+      <div>
+        {homeSlider.map(e=>(
+          <img src={e.url} alt="img" className='w-full h-[380px] md:h-[600px] ' />
+        ))}
+      </div>
     </>
   )
 }

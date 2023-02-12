d0d34084237444bda73433bb9a6b45ec77153a31
rahulsuvaidyam <rahul.suvaidyam@gmail.com>
2023-02-12 12:20:47 +0530

    change img in home page

--- src/Components/Home/Slider/Slider.js
+++ src/Components/Home/Slider/Slider.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import slider from '../../../Assets/home-img.jpg'
 
 const Slider = () => {
   const homeSlider = [
     {
-      url: "https://static.wixstatic.com/media/89ec68_f16e2fa1d2cb4eeba61e49ff0e51a299~mv2.jpg/v1/fill/w_882,h_601,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/89ec68_f16e2fa1d2cb4eeba61e49ff0e51a299~mv2.jpg"
+      url: slider
     },
   ]
   return (
     <>
-      <div>
+      <div className='mt-20'>
         {homeSlider.map(e=>(
-          <img src={e.url} alt="img" className='w-full h-[380px] md:h-[600px] ' />
+          <img src={e.url} alt="img" className='w-full h-[200px] md:h-[460px] ' />
         ))}
       </div>
     </>

8d7cabb680b3daf7814e6ce422cb5a087a9ef425
Samar Raj <samar.suvaidyam@gmail.com>
2024-02-25 00:10:22 +0530

    resolved conflict

--- src/Components/Router/Router.js
+++ src/Components/Router/Router.js
@@ -12,16 +12,16 @@ const Router = () => {
   return (
     <div className='min-h-screen flex flex-col'>
       <BrowserRouter>
-      <Header/>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<AboutUs />} />
-            <Route path="/work" element={<Work />} />
-            {/* <Route path="/teams" element={<Teams />} /> */}
-            <Route path="/contact" element={<ContactUs />} />
-            <Route path="/involved" element={<GetInvolved />} />
-          </Routes>
-          <Footer/>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/work" element={<Work />} />
+          {/* <Route path="/teams" element={<Teams />} /> */}
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/involved" element={<GetInvolved />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
